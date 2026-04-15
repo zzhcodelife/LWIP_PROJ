51db440b52e9d3cd6f51981e021aec7cbcafc843
zzhcodelife <zihanzhang886@gmail.com>
2026-04-15 23:32:57 +0800

    Lwip_0415_Lwip_RTOS_netconn

---
+++ 01_Lwip_nosys/Lwip_nosys/User/client.c
@@ -0,0 +1,41 @@
+#include "client.h"
+#include "lwip/opt.h"
+#include "lwip/sys.h"
+#include "lwip/api.h"
+static void client(void *thread_param)
+{
+    struct netconn *conn;
+    int ret;
+    ip4_addr_t ipaddr;
+    uint8_t send_buf[]= "This is a TCP Client test...\n";
+    while (1)
+    {
+        conn = netconn_new(NETCONN_TCP); 
+        if (conn == NULL)
+        {
+            //printf("create conn failed!\n");
+            vTaskDelay(10);
+            continue;
+        }
+        IP4_ADDR(&ipaddr,192,168,0,181); 
+        ret = netconn_connect(conn,&ipaddr,5001); 
+        if (ret == -1)
+        {
+           // printf("Connect failed!\n");
+            netconn_close(conn); 
+            vTaskDelay(10);
+            continue;
+        }
+        //printf("Connect to iperf server successful!\n");
+        while (1)
+        {
+            ret = netconn_write(conn,send_buf,sizeof(send_buf),0); (5)
+            vTaskDelay(1000);
+        }
+    }
+}
+void
+    client_init(void)
+{
+    sys_thread_new("client", client, NULL, 512, 4);
+}
---
+++ 01_Lwip_nosys/Lwip_nosys/User/client.h
@@ -0,0 +1,4 @@
+#ifndef _CLIENT_H
+#define _CLIENT_H
+void client_init(void);
+#endif /* _CLIENT_H */
