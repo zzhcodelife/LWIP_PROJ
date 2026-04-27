80856ce1775531dd2009a2227f46ba70dc61d295
zzhcodelife <zihanzhang886@gmail.com>
2026-04-27 10:39:01 +0800

    RAW_v1_fail

--- 01_Lwip_nosys/Lwip_nosys/User/main.c
+++ 01_Lwip_nosys/Lwip_nosys/User/main.c
@@ -34,6 +34,7 @@
 #include "lwip/ip.h"
 #include "lwip/snmp.h"
 #include "lwip/timeouts.h"
+#include "tcp_client_raw.h"
 struct netif gnetif;
 ip4_addr_t ipaddr;
 ip4_addr_t netmask;
@@ -90,7 +91,7 @@ int main(void)
 
   // LwIP协议栈初始化
   LwIP_Init();
-
+  TCP_Client_Raw_Init();
   while (1)
   {
     if (flag)
---
+++ 01_Lwip_nosys/Lwip_nosys/User/tcp_client_raw.h
@@ -0,0 +1,5 @@
+#ifndef _TCPCLIENT_H_
+#define _TCPCLIENT_H_
+#define TCP_CLIENT_PORT 6009
+void TCP_Client_Raw_Init(void);
+#endif
