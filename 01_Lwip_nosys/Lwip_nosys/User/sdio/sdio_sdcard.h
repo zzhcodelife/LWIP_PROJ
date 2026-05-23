fafccc6dfeaa6612eb476c7d30c935ecd54f5494
zzhcodelife <zihanzhang886@gmail.com>
2026-05-23 22:29:33 +0800

    Lwip_0523_SD_test1

--- 01_Lwip_nosys/Lwip_nosys/User/sdio/sdio_sdcard.h
+++ 01_Lwip_nosys/Lwip_nosys/User/sdio/sdio_sdcard.h
@@ -1,30 +1,27 @@
 #ifndef _SDMMC_SDCARD_H
 #define _SDMMC_SDCARD_H
-#include "sys.h"
-//////////////////////////////////////////////////////////////////////////////////	 
-//本程序只供学习使用，未经作者许可，不得用于其它任何用途
-//ALIENTEK STM32F746开发板
-//SD卡驱动代码	   
-//正点原子@ALIENTEK
-//技术论坛:www.openedv.com
-//创建日期:2016/1/6
-//版本：V1.0
-//版权所有，盗版必究。
-//Copyright(C) 广州市星翼电子科技有限公司 2014-2024
-//All rights reserved									  
-//////////////////////////////////////////////////////////////////////////////////
 
-#define SD_TIMEOUT ((uint32_t)100000000)            //超时时间
+#include <stdint.h>
+#include "stm32f4xx_hal.h"
 
-#define SD_DMA_MODE    		0	//1：DMA模式，0：查询模式   
+#define SD_TIMEOUT   ((uint32_t)100000000U)
+#define SD_DMA_MODE  1U   /* 1：DMA模式，0：查询模式 */
+#define SD_OK        0U
 
-extern SD_HandleTypeDef        SDCARD_Handler;     //SD卡句柄
-extern HAL_SD_CardInfoTypedef  SDCardInfo;         //SD卡信息结构体
+/* 正点原子示例里的卡类型判断（与 HAL CardType 数值兼容） */
+#define STD_CAPACITY_SD_CARD_V1_1  CARD_SDSC
+#define STD_CAPACITY_SD_CARD_V2_0  1U
+#define HIGH_CAPACITY_SD_CARD      CARD_SDHC_SDXC
+#define MULTIMEDIA_CARD            CARD_SECURED
 
-u8 SD_Init(void);
-u8 SD_GetCardInfo(HAL_SD_CardInfoTypedef *cardinfo);
-u8 SD_ReadDisk(u8* buf,u32 sector,u32 cnt);
-u8 SD_WriteDisk(u8 *buf,u32 sector,u32 cnt);
-u8 SD_ReadBlocks_DMA(uint32_t *buf,uint64_t sector,uint32_t blocksize,uint32_t cnt);
-u8 SD_WriteBlocks_DMA(uint32_t *buf,uint64_t sector,uint32_t blocksize,uint32_t cnt);
+extern SD_HandleTypeDef       SDCARD_Handler;
+extern HAL_SD_CardInfoTypeDef SDCardInfo;
+
+uint8_t SD_Init(void);
+uint8_t SD_GetCardInfo(HAL_SD_CardInfoTypeDef *cardinfo);
+uint8_t SD_ReadDisk(uint8_t *buf, uint32_t sector, uint32_t cnt);
+uint8_t SD_WriteDisk(uint8_t *buf, uint32_t sector, uint32_t cnt);
+uint8_t SD_ReadBlocks_DMA(uint32_t *buf, uint64_t sector, uint32_t blocksize, uint32_t cnt);
+uint8_t SD_WriteBlocks_DMA(uint32_t *buf, uint64_t sector, uint32_t blocksize, uint32_t cnt);
+
 #endif
